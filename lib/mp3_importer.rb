f34c36801485bdcfc51b54421fca11796fe9b69b
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-30 18:23:10 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,7 +17,7 @@ class MP3Importer
   end
 
   def import
-    @files.each { |file| Song.new_by_filename(file)  }
+    @files.each { |file| x = Song.new_by_filename(file) binding.pry  }
   end
 
 end
