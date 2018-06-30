da526a82c9764dd7b7d71d91c281840a4d6703ae
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-30 17:52:22 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files_array = []
+    Dir[@path + "/*"].each{|f| files_array << f.math(/\/\D+\.mp3/)}
+    puts files_array
+    binding.pry
+  end
+
+  def import
+  end
+
+end
