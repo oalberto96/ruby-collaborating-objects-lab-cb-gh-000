659f7919b323db3635c587f66864b6f8d37b673b
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-30 18:14:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     artist_song = file_name.split(" - ")
     binding.pry
     artist_name = artist_song.first
-    song_name = artist_song.last
+    song_name = artist_song.[1]
     artist = Artist.find_or_create_by_name(artist_name)
     song = self.new(song_name)
   end
